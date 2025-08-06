23b19369c55451fae923c26cce089a75b527bf3f
Egi Sutedi <32353388+egisutedi@users.noreply.github.com>
2025-08-07 01:59:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,28 +5,18 @@
     <div class="post">
         <a href="<?php the_permalink(); ?>">
             <?php 
-            // Ambil video ID dari post_meta
-            $video_id = get_post_meta(get_the_ID(), 'xhijab_video_id', true);
-
             if (has_post_thumbnail()) {
-                // Kalau ada featured image, pakai itu
                 the_post_thumbnail('medium');
-            } elseif ($video_id) {
-                // Kalau ada video_id dari Doodstream
-                echo '<img src="https://img.doodcdn.co/splash/'.$video_id.'.jpg" alt="Video Thumbnail">';
             } else {
-                // Kalau tidak ada gambar sama sekali
                 echo '<img src="https://via.placeholder.com/300x200?text=No+Image">';
             }
             ?>
         </a>
         
-        <!-- Judul Post -->
         <div class="post-title">
             <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
         </div>
 
-        <!-- Views & Likes -->
         <div style="font-size:13px; color:#666; margin-top:5px;">
             👁 <?php echo function_exists('xhijab_get_post_views') ? xhijab_get_post_views(get_the_ID()) : 0; ?> views
             &nbsp; | &nbsp;
